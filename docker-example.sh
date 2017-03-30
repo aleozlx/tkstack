ec3a35fcb27a9b4711df6a145efef16b0fadc88c
Alex Yang <aleozlx@gmail.com>
2017-03-30 15:31:17 -0500

    + docker-example.sh

---
+++ docker-example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOCKER="nvidia-docker"
+DOCKER_RUN="-it"
+IMAGE="aleozlx/tkstack:t101k202-gpu"
+
+# Expose ports
+let "P=8888"
+
+PORTS="-p $((P++)):8888"
+
+# Define file locations to be mounted from host here
+PROJECT="$(readlink -f .)"
+SCRIPTS="$(readlink -f ./notebooks)"
+DATASETS="$(readlink -f ../datafiles)"
+
+VOLUMNS="-v $PROJECT:/workspace/project -v $DATASETS:/workspace/datasets -v $SCRIPTS:/workspace/scripts"
+
+# Launch
+set -x
+$DOCKER run $DOCKER_RUN $PORTS $VOLUMNS $IMAGE $DOCKER_CMD
